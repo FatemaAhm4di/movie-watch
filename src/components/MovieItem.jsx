4f5d47334b3252ad1740ad8829ff11a4c6717888
ꜰᴀᴛᴇᴍᴀ ᴀʜᴍᴀᴅɪ <fatema.ahmadi1384@gmail.com>
2026-02-12 15:20:50 +0430

    change color button

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,6 +1,6 @@
 export default function MovieItem({ movie, onToggle, onDelete }) {
     return (
-        <li style={{ padding: '8px', borderBottom: '1px solid #eee' }}>
+        <li style={{ padding: '8px', borderBottom: '5px solid #658b28' }}>
             <strong>{movie.title}</strong> ({movie.genre}) -
             {movie.watched ? '✅ Watched' : '❌ Unwatched'}
             <button onClick={() => onToggle(movie.id)} style={{ marginLeft: '8px', }}>Toggle</button>
